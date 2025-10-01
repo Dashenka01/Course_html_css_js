49d6bd5327a57d0eb89b9a6271e146b795158ada
Dashenka01 <167013030+Dashenka01@users.noreply.github.com>
2025-10-01 08:38:43 +0500

    Выполнила 5 домашнее задание

---
+++ homework_5_java_script.js
@@ -0,0 +1,17 @@
+let username;
+username = "my name";
+
+ let bonusBalance;
+ bonusBalance = 1000;
+
+ console.log("Пользователь " + username) 
+ console.log("Баланс " + bonusBalance) 
+
+
+let a;
+a =  50;
+
+let b;
+b = 3;
+
+console.log(a * 4 - b * 7 + bonusBalance)
